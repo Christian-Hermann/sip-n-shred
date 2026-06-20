7059ea39ca4538560f355530c69c9d45f73c109f
Your NameChristian <christianhermanndev@gmail.com>
2026-06-20 12:44:41 -0500

    Clean up initial homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Sip n' Shred</h1>
-      <p>Compare Utah skit resorts, conditions, and aprés-ski spots.</p>
+      <p>Compare Utah ski resorts, conditions, and après-ski spots.</p>
     </>
   );
 }
