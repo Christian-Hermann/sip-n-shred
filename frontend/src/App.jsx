ea5abedb618aea7b1db388b2cbfeab4861ce3e83
Your NameChristian <christianhermanndev@gmail.com>
2026-06-20 13:26:27 -0500

    Add conditions data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import conditions from "./data/conditions";
 
 function App() {
   return (
     <>
       <h1>Sip n' Shred</h1>
       <p>Compare Utah ski resorts, conditions, and après-ski spots.</p>
+
+      <h2>Today's Conditions</h2>
+
+      {conditions.map((condition) => (
+        <div key={condition.id}>
+          <p>
+            {condition.resort} - {condition.newSnow}" New Snow
+          </p>
+        </div>
+      ))}
     </>
   );
 }
---
+++ frontend/src/data/conditions.js
@@ -0,0 +1,29 @@
+const conditions = [
+  {
+    id: 1,
+    resort: "Park City",
+    newSnow: 8,
+  },
+  {
+    id: 2,
+    resort: "Deer Valley",
+    newSnow: 8,
+  },
+  {
+    id: 3,
+    resort: "Snowbird",
+    newSnow: 10,
+  },
+  {
+    id: 4,
+    resort: "Powder Mountain",
+    newSnow: 15,
+  },
+  {
+    id: 5,
+    resort: "Sundance",
+    newSnow: 8,
+  },
+];
+
+export default conditions;
