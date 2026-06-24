7e21575a20bd5128763d9b33af8d0c1117ed7384
Your NameChristian <christianhermanndev@gmail.com>
2026-06-24 13:18:53 -0500

    Add drive time and apres to card

--- frontend/src/components/ConditionCard.jsx
+++ frontend/src/components/ConditionCard.jsx
@@ -5,6 +5,26 @@ function ConditionCard({ condition }) {
     <div className="condition-card">
       <h3>{condition.resort}</h3>
       <p>{condition.newSnow}" New Snow</p>
+
+      <p>
+        <strong>Difficulty: </strong>
+        {condition.difficulty}
+      </p>
+
+      <p>
+        <strong>Best For: </strong>
+        {condition.bestFor}
+      </p>
+
+      <p>
+        <strong>DriveTime: </strong>
+        {condition.driveTime} min
+      </p>
+
+      <p>
+        <strong>Après Rating: </strong>
+        {condition.apresRating}/10
+      </p>
     </div>
   );
 }
